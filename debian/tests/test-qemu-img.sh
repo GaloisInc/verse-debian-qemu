d68e9ed921a78e9f8611fae67bc7ca6724b36cc3
Michael Tokarev <mjt@tls.msk.ru>
2022-05-07 00:01:37 +0300

    d/tests/: rewrite test-qemu-img.sh

--- debian/tests/test-qemu-img.sh
+++ debian/tests/test-qemu-img.sh
@@ -2,25 +2,34 @@
 
 set -e
 
-RAW_IMG="${AUTOPKGTEST_TMP}/q.img"
+cd "$AUTOPKGTEST_TMP"
 
-echo -n "Testing if qemu-img creates images..."
-qemu-img create "${RAW_IMG}" 12G
-echo "done."
+doit() {
+ echo "$1:"
+  shift
+  echo "$1"
+  if [ -n "$2" ]; then
+    out=$($1)
+    eval "case \"\$out\" in ( $2 ) echo \"\$out\";; (*) echo \"unexpected output:\"; echo \" want $2\"; echo \"  got \$out\"; return 1;; esac"
+  else
+    $1
+  fi
+  echo ok.
+}
 
-echo -n "Testing for correct image size..."
-ls -l "${RAW_IMG}" | grep -qs " 12884901888 "
-echo "done."
-echo -n "Testing if file is sparse..."
-ls -s "${RAW_IMG}" | grep -qs "^0 "
-echo "done."
+doit "Testing if qemu-img creates images" \
+	"qemu-img create q.raw 12G"
 
-QCOW2_IMG="${AUTOPKGTEST_TMP}/q.qcow2"
-echo "Testing if conversion to a qcow2 image works..."
-qemu-img convert -Oqcow2 "${RAW_IMG}" "${QCOW2_IMG}"
-echo "done."
+doit "Testing for correct image size" \
+	"ls -l q.raw" '*\ 12884901888\ *'
 
-echo "Testing if image is qcow2 works..."
-file "${QCOW2_IMG}" | grep "QEMU QCOW Image .* 12884901888"
-echo "done."
+doit "Testing if file is sparse" \
+	'ls -s q.raw' '[04]\ *'
 
+doit "Testing if conversion to a qcow2 image works" \
+	"qemu-img convert -f raw -O qcow2 q.raw q.qcow2"
+
+doit "Checking if image is qcow2" \
+	'file q.qcow2' '*QEMU\ QCOW2\ Image\ *\ 12884901888\ *'
+
+rm -f q.raw q.qcow2
